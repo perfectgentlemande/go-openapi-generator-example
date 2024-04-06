87c4454f708d36163b21832e93aa19eff4400a41
Артем Чаадаев <perfectgentleman.de@ya.ru>
2024-04-06 21:30:49 +0400

    code moved to controller.go

---
+++ go-openapi-check/controller.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	openapi "github.com/perfectgentlemande/go-openapi-generator-example/openapi"
+)
+
+type Controller struct {
+}
+
+func (c *Controller) UserGet(ctx context.Context, limit int64, offset int64) (openapi.ImplResponse, error) {
+	return openapi.ImplResponse{
+		Body: []openapi.User{
+			{
+				Id:       uuid.NewString(),
+				Username: "alice01",
+			},
+			{
+				Id:       uuid.NewString(),
+				Username: "bob01",
+			},
+		},
+		Code: 200,
+	}, nil
+}
+func (c *Controller) UserIdDelete(ctx context.Context, id string) (openapi.ImplResponse, error) {
+	return openapi.ImplResponse{
+		Body: openapi.User{
+			Id:       uuid.NewString(),
+			Username: "alice01",
+		},
+		Code: 200,
+	}, nil
+}
+func (c *Controller) UserIdGet(ctx context.Context, id string) (openapi.ImplResponse, error) {
+	return openapi.ImplResponse{
+		Body: openapi.User{
+			Id:       uuid.NewString(),
+			Username: "alice01",
+		},
+		Code: 200,
+	}, nil
+}
+func (c *Controller) UserIdPut(ctx context.Context, id string) (openapi.ImplResponse, error) {
+	return openapi.ImplResponse{
+		Body: openapi.User{
+			Id:       uuid.NewString(),
+			Username: "alice01",
+		},
+		Code: 200,
+	}, nil
+}
+func (c *Controller) UserPost(ctx context.Context) (openapi.ImplResponse, error) {
+	return openapi.ImplResponse{
+		Body: openapi.User{
+			Id:       uuid.NewString(),
+			Username: "alice01",
+		},
+		Code: 200,
+	}, nil
+}
--- go-openapi-check/main.go
+++ go-openapi-check/main.go
@@ -19,65 +19,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/google/uuid"
 	openapi "github.com/perfectgentlemande/go-openapi-generator-example/openapi"
 	"github.com/rs/zerolog"
 )
-
-type Controller struct {
-}
-
-func (c *Controller) UserGet(ctx context.Context, limit int64, offset int64) (openapi.ImplResponse, error) {
-	return openapi.ImplResponse{
-		Body: []openapi.User{
-			{
-				Id:       uuid.NewString(),
-				Username: "alice01",
-			},
-			{
-				Id:       uuid.NewString(),
-				Username: "bob01",
-			},
-		},
-		Code: 200,
-	}, nil
-}
-func (c *Controller) UserIdDelete(ctx context.Context, id string) (openapi.ImplResponse, error) {
-	return openapi.ImplResponse{
-		Body: openapi.User{
-			Id:       uuid.NewString(),
-			Username: "alice01",
-		},
-		Code: 200,
-	}, nil
-}
-func (c *Controller) UserIdGet(ctx context.Context, id string) (openapi.ImplResponse, error) {
-	return openapi.ImplResponse{
-		Body: openapi.User{
-			Id:       uuid.NewString(),
-			Username: "alice01",
-		},
-		Code: 200,
-	}, nil
-}
-func (c *Controller) UserIdPut(ctx context.Context, id string) (openapi.ImplResponse, error) {
-	return openapi.ImplResponse{
-		Body: openapi.User{
-			Id:       uuid.NewString(),
-			Username: "alice01",
-		},
-		Code: 200,
-	}, nil
-}
-func (c *Controller) UserPost(ctx context.Context) (openapi.ImplResponse, error) {
-	return openapi.ImplResponse{
-		Body: openapi.User{
-			Id:       uuid.NewString(),
-			Username: "alice01",
-		},
-		Code: 200,
-	}, nil
-}
 
 func main() {
 	log := zerolog.New(os.Stdout).With().Timestamp().Logger()
